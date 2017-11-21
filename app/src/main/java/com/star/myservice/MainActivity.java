d51f03c09f26534c7c511feea7a549f3cfd29e17
star <s0913940588@gmail.com>
2017-11-21 13:25:44 +0800

    ...

--- app/src/main/java/com/star/myservice/MainActivity.java
+++ app/src/main/java/com/star/myservice/MainActivity.java
@@ -1,7 +1,9 @@
 package com.star.myservice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.print("MainActivity.onCreate");
     }
+
+    public void onStartService(View v)
+    {
+        System.out.print("MainActivity.onStartService");
+        Intent intent = new Intent(this , MyService.class);
+        MyService.isStop = false;
+        startService(intent);
+    }
+
+    public void onStopService(View v)
+    {
+        System.out.print("MainActivity.onStopService");
+        Intent intent = new Intent(this , MyService.class);
+        MyService.isStop = true;
+        stopService(intent);
+    }
+
 }
--- app/src/main/java/com/star/myservice/MyService.java
+++ app/src/main/java/com/star/myservice/MyService.java
@@ -5,6 +5,11 @@ import android.content.Intent;
 import android.os.IBinder;
 
 public class MyService extends Service {
+
+    public static boolean isStop = true;
+    private int count;
+    private Thread t;
+
     public MyService() {
     }
 
@@ -13,4 +18,46 @@ public class MyService extends Service {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
     }
+
+    @Override
+    public void onCreate() {
+        System.out.print("MyService.onCreate");
+        isStop = false;
+        t = new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    if (!isStop) {
+                        try {
+                            Thread.sleep(1000);
+                            System.out.print("count = " + count);
+                            count++;
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        System.out.println("結束服務");
+                        break;
+                    }
+                }
+            }
+        };
+        t.start();
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        isStop = true;
+        t = null;
+        super.onDestroy();
+    }
+
+
 }
